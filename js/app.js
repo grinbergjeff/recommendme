5a4796793ab36e68e27232ce110bca2c04d6557a
Jeffrey Grinberg <grinbergjeff@gmail.com>
2015-07-18 15:11:17 -0400

    got form submit to work

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
 //JQuery Code:
 $(document).ready(function() {
-	//When bouncing arrow is pressed, browser scrolls to next section
+	downarrow();
+	whatuserlikes();
+});
+//When bouncing arrow is pressed, browser scrolls to next section
+function downarrow() {
 	$('#down').click(function() {
 		$('body, html').animate({
 			scrollTop: $('#wdyl').offset().top}, 1000);
 	});
-});
-
-
+}
+function whatuserlikes() {
+	$('#user-input').keydown(function(ent) {
+		if (ent.which == 13) {
+			ent.preventDefault();
+			var userlikes = $('#user-input').val();
+			console.log('user likes: ' + userlikes);
+			$(this).val('');
+			
+		}
+	});
+}
+/*
 //Javascript Code:
 window.onload = function(){ 
-
-};
+	/*document.getElementById('stmt').innerHTML('What [] do you like? TEST');
+};*/
